16c92f067f052432173cd4d8395d7293c076afdb
GarySu <Gary.Su@hearst.com>
2019-09-30 11:16:16 -0400

    Adding CrossOrigin to controller

--- server/src/main/java/com/hearst/pwdemo/controller/GroupController.java
+++ server/src/main/java/com/hearst/pwdemo/controller/GroupController.java
@@ -16,6 +16,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class GroupController {
     private final Logger log = LoggerFactory.getLogger(GroupController.class);
     private GroupRepository groupRepository;
